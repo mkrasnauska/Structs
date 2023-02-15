91d15d26ce2dd609783f6d41d47c0c3aa4bf87d9
Margarita <margaritakrasnauska@gmail.com>
2023-02-15 13:50:07 +0000

    Add project files.

---
+++ Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Structs
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number num1;
+            num1.Amount = 5.323422123m;
+            Console.WriteLine("The Decimal Number is:" + "\n" + num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
